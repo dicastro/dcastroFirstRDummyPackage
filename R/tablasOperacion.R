a68a647e3576cba03aceca227bdf8596097601da
dicastro <diegocastroviadero19@gmail.com>
2019-01-25 09:24:49 +0100

    renamed columns id and nombre to be camel case (Id, Nombre)

--- R/tablasOperacion.R
+++ R/tablasOperacion.R
@@ -11,19 +11,19 @@
         for (capitulo_i in 1:nrow(capitulos_padre)) {
             capitulo <- capitulos_padre[capitulo_i, ]
 
-            tablas_capitulo_raw <- readLines(paste("http://servicios.ine.es/wstempus/js/ES/TABLAS_CAPITULO/", capitulo$id, sep = ""))
+            tablas_capitulo_raw <- readLines(paste("http://servicios.ine.es/wstempus/js/ES/TABLAS_CAPITULO/", capitulo$Id, sep = ""))
             tablas_capitulo <- fromJSON(paste(tablas_capitulo_raw, collapse = ""))
 
             if (length(tablas_capitulo) > 0) {
-                new_tablas <- data.frame(id = sapply(tablas_capitulo, function(x) x$Id), nombre = sapply(tablas_capitulo, function(x) x$Nombre), padre = capitulo$id, es_tabla = TRUE)
+                new_tablas <- data.frame(Id = sapply(tablas_capitulo, function(x) x$Id), Nombre = sapply(tablas_capitulo, function(x) x$Nombre), Padre = capitulo$Id, es_tabla = TRUE)
                 new_cum_tablas <- rbind(new_cum_tablas, new_tablas)
             }
 
-            subcapitulos_raw <- readLines(paste("http://servicios.ine.es/wstempus/js/ES/CAPITULOS/", capitulo$id, sep = ""))
+            subcapitulos_raw <- readLines(paste("http://servicios.ine.es/wstempus/js/ES/CAPITULOS/", capitulo$Id, sep = ""))
             subcapitulos <- fromJSON(paste(subcapitulos_raw, collapse = ""))
 
             if (length(subcapitulos) > 0) {
-                c <- data.frame(id = sapply(subcapitulos, function(x) x$Id), nombre = sapply(subcapitulos, function(x) x$Nombre), padre = capitulo$id, es_tabla = FALSE)
+                c <- data.frame(Id = sapply(subcapitulos, function(x) x$Id), Nombre = sapply(subcapitulos, function(x) x$Nombre), Padre = capitulo$Id, es_tabla = FALSE)
                 new_cum_tablas <- rbind(new_cum_tablas, .masTablasOperacion(c))
                 new_cum_tablas <- rbind(new_cum_tablas, c)
             }
@@ -39,8 +39,8 @@ tablasOperacion <- function(operacionId) {
     capitulos_raiz_raw <- readLines(paste("http://servicios.ine.es/wstempus/js/ES/CAPITULOSRAIZ_OPERACION/", operacionId, sep = ""))
     capitulos_raiz <- fromJSON(paste(capitulos_raiz_raw, collapse = ""))
 
-    cr <- data.frame(id = sapply(capitulos_raiz, function(x) x$Id), nombre = sapply(capitulos_raiz, function(x) x$Nombre), padre = NA, es_tabla = FALSE)
+    cr <- data.frame(Id = sapply(capitulos_raiz, function(x) x$Id), Nombre = sapply(capitulos_raiz, function(x) x$Nombre), Padre = NA, es_tabla = FALSE)
     cr <- .masTablasOperacion(cr)
 
-    cr[cr$es_tabla == TRUE, ]
+    cr[cr$es_tabla == TRUE, c("Id", "Nombre")]
 }
